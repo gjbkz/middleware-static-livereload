2aeb01ee849cc00c53c1f3cfac26b7e1c3238441
Kei Ito <kei.itof@gmail.com>
2017-05-09 15:25:10 +0900

    rewrite tests

--- test/middleware-static-livereload.js
+++ test/middleware-static-livereload.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const http = require('http');
 const connect = require('connect');
 const readStream = require('j1/readStream');
+const middleware = require('..');
 
 describe('middleware-static-livereload', function () {
 	const documentRoot = path.join(__dirname, 'doc');
@@ -19,70 +20,64 @@ describe('middleware-static-livereload', function () {
 
 	before(function () {
 		server = connect()
-			.use(require('../middleware-static-livereload')({
-				documentRoot: documentRoot,
-				livereload: {port: livereloadPort},
-				onStartWatcher: (startedWatcher) => {
-					watcher = startedWatcher;
-				}
-			}))
-			.listen(serverPort);
+		.use(middleware({
+			documentRoot: documentRoot,
+			livereload: {port: livereloadPort},
+			onStartWatcher: (startedWatcher) => {
+				watcher = startedWatcher;
+			}
+		}))
+		.listen(serverPort);
 	});
 
 	after(function () {
 		server.close();
 	});
 
-	it('should inject a script tag', function () {
+	it('should inject a script tag', async function () {
 		const expectedHTML = [
 			'<!doctype html>',
 			`<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':${livereloadPort}/livereload.js?snipver=1"></' + 'script>')</script>`,
 			'<title>middleware-static-livereload</title>',
 			''
 		].join('\n');
-		return new Promise((resolve) => {
+		const res = await new Promise((resolve) => {
 			http.get(getURL('/'), resolve);
-		})
-			.then(readStream)
-			.then((receivedHTML) => {
-				assert.equal(receivedHTML, expectedHTML);
-			});
+		});
+		const receivedHTML = await readStream(res);
+		assert.equal(receivedHTML, expectedHTML);
 	});
 
-	it('should redirect to / if a directory is requested', function () {
-		return new Promise((resolve) => {
+	it('should redirect to / if a directory is requested', async function () {
+		const res = await new Promise((resolve) => {
 			http.get(getURL('/directory'), resolve);
-		})
-			.then((res) => {
-				assert.equal(res.statusCode, HTTPREDIRECT);
-				assert.equal(res.headers.location, '/directory/');
-			});
+		});
+		assert.equal(res.statusCode, HTTPREDIRECT);
+		assert.equal(res.headers.location, '/directory/');
 	});
 
-	it('should behave as a static file server', function () {
+	it('should behave as a static file server', async function () {
 		const expectedJS = [
 			'console.log(\'app.js\');',
 			''
 		].join('\n');
-		return new Promise((resolve) => {
+		const res = await new Promise((resolve) => {
 			http.get(getURL('/app.js'), resolve);
-		})
-			.then(readStream)
-			.then((receivedJS) => {
-				assert.equal(receivedJS, expectedJS);
-			});
+		});
+		const receivedJS = await readStream(res);
+		assert.equal(receivedJS, expectedJS);
 	});
 
 	it('should watch the files', function (done) {
 		watcher
-			.once('all', (eventType, filePath) => {
-				try {
-					assert.deepEqual([eventType, filePath], ['change', path.join(documentRoot, 'app.js')]);
-					done();
-				} catch (error) {
-					done(error);
-				}
-			});
+		.once('all', (eventType, filePath) => {
+			try {
+				assert.deepEqual([eventType, filePath], ['change', path.join(documentRoot, 'app.js')]);
+				done();
+			} catch (error) {
+				done(error);
+			}
+		});
 		fs.utimes(path.join(documentRoot, 'app.js'), NaN, NaN, (error) => {
 			if (error) {
 				done(error);
