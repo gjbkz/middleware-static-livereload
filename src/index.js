1b49ec36a539b4c48433e1b7bfb7e1ced0fb2b4a
Kei Ito <kei.itof@gmail.com>
2018-04-12 02:04:51 +0900

    rewrite tests

---
+++ src/index.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const livereload = require('livereload');
+const {promisify} = require('@nlib/util');
+const {ContentType} = require('@nlib/content-type');
+const {ReplaceStream} = require('@nlib/replace-stream');
+const readFile = promisify(fs.readFile, fs);
+const stat = promisify(fs.stat, fs);
+
+module.exports = function middleware({
+	documentRoot = process.cwd(),
+	livereload: livereloadOption = {},
+	contentType,
+	include = [/\.html?$/],
+	exclude = [],
+	pattern = /<!doctype\s+html\s*[^<>]*>/i,
+} = {}) {
+	include = Array.isArray(include) ? include : [include];
+	exclude = Array.isArray(exclude) ? exclude : [exclude];
+	contentType = new ContentType(contentType);
+	let server;
+	const waitListening = new Promise((resolve) => {
+		server = livereload.createServer(livereloadOption, resolve);
+	})
+	.then(() => server.server._server.address().port);
+	const watcher = server.watch(documentRoot);
+	return Object.assign(
+		function (req, res, next) {
+			const parsed = url.parse(req.url);
+			const filePath = path.join(documentRoot, parsed.pathname.replace(/\/$/, '/index.html'));
+			Promise.all([
+				stat(filePath),
+				waitListening,
+			])
+			.then(([stats, port]) => {
+				if (stats.isDirectory()) {
+					const newURL = Object.assign({}, parsed);
+					newURL.pathname += '/';
+					res.writeHead(301, {'location': url.format(newURL)});
+					res.end();
+					return;
+				}
+				if (include.some((regexp) => regexp.test(filePath)) && exclude.every((regexp) => !regexp.test(filePath))) {
+					res.writeHead(200, {'content-type': contentType.get(filePath)});
+					return new Promise((resolve, reject) => {
+						fs.createReadStream(filePath)
+						.pipe(new ReplaceStream([{
+							pattern,
+							replacement: (match) => `${match}\n<script>document.write('<script src="http://'+(location.host||'localhost').split(':')[0]+':${port}/livereload.js?snipver=1"></'+'script>')</script>`,
+						}]))
+						.pipe(res)
+						.once('error', reject)
+						.once('finish', resolve);
+					});
+				}
+				res.writeHead(200, {
+					'content-type': contentType.get(filePath),
+					'content-length': `${stats.size}`,
+				});
+				return new Promise((resolve, reject) => {
+					fs.createReadStream(filePath)
+					.pipe(res)
+					.once('error', reject)
+					.once('finish', resolve);
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+				next();
+			});
+		},
+		{server, watcher}
+	);
+};
---
+++ test/index.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const fs = require('fs');
+const http = require('http');
+const connect = require('connect');
+const test = require('@nlib/test');
+const middleware = require('..');
+const readStream = (stream) => new Promise((resolve, reject) => {
+	const chunks = [];
+	stream
+	.once('error', reject)
+	.on('data', (chunk) => chunks.push(chunk))
+	.on('end', () => resolve(Buffer.concat(chunks)));
+});
+
+test('middleware-static-livereload', (test) => {
+	const documentRoot = path.join(__dirname, 'doc');
+	const serverPort = 7654;
+	const livereloadPort = 7655;
+	const getURL = (pathname = '') => `http://127.0.0.1:${serverPort}${pathname}`;
+	const results = {};
+
+	test('create a middleware function', () => {
+		results.middleware = middleware({
+			documentRoot,
+			livereload: {port: livereloadPort},
+		});
+	});
+
+	test('start a server', () => {
+		results.server = connect()
+		.use(results.middleware)
+		.listen(serverPort);
+	});
+
+	test('inject a script tag', async (test) => {
+		const expectedHTML = [
+			'<!doctype html>',
+			`<script>document.write('<script src="http://'+(location.host||'localhost').split(':')[0]+':${livereloadPort}/livereload.js?snipver=1"></'+'script>')</script>`,
+			'<title>middleware-static-livereload</title>',
+			'',
+		].join('\n');
+		const res = await new Promise((resolve) => http.get(getURL('/'), resolve));
+		const receivedHTML = `${await readStream(res)}`;
+		test.compare(receivedHTML, expectedHTML);
+	});
+
+	test('redirect to / if a directory is requested', async (test) => {
+		const res = await new Promise((resolve) => http.get(getURL('/directory'), resolve));
+		test.compare(res, {
+			statusCode: 301,
+			headers: {location: '/directory/'},
+		});
+	});
+
+	test('behave as a static file server', async (test) => {
+		const expectedJS = [
+			'console.log(\'app.js\');',
+			'',
+		].join('\n');
+		const res = await new Promise((resolve) => http.get(getURL('/app.js'), resolve));
+		const receivedJS = `${await readStream(res)}`;
+		test.compare(receivedJS, expectedJS);
+	});
+
+	test('watch the files', (test) => {
+		return new Promise((resolve, reject) => {
+			results.middleware.watcher
+			.once('all', (eventType, filePath) => {
+				try {
+					test.compare({eventType, filePath}, {
+						eventType: 'change',
+						filePath: path.join(documentRoot, 'app.js'),
+					});
+					resolve();
+				} catch (error) {
+					reject(error);
+				}
+			});
+			fs.utimes(path.join(documentRoot, 'app.js'), new Date(), new Date(), (error) => {
+				if (error) {
+					reject(error);
+				}
+			});
+		});
+	});
+
+	test('close the livereload server', () => results.middleware.server.close());
+	test('close the livereload watcher', () => results.middleware.watcher.close());
+	test('close the server', () => new Promise((resolve) => results.server.close(resolve)));
+
+});
