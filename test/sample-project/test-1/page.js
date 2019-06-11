3054ff4ef768c003a7f175d020fbef90a40e540d
Kei Ito <kei.itof@gmail.com>
2019-06-11 15:37:26 +0900

    chore: add test-3

--- test/sample-project/test-1/page.js
+++ test/sample-project/test-1/page.js
@@ -1,5 +1,5 @@
-document.title = 'passed: test-1';
 const timerId = setInterval(function () {
+    document.title = 'passed: test-1';
     const style = getComputedStyle(document.body);
     if (style.getPropertyValue('animation-name') === 'test-2') {
         document.title = 'passed: test-2';
