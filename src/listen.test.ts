4041773fcd862062e2893c3ad99aba7de55f774e
Kei Ito <kei.itof@gmail.com>
2019-06-28 11:48:27 +0900

    test: add a test for listen

---
+++ src/listen.test.ts
@@ -0,0 +1,25 @@
+import * as http from 'http';
+import test from 'ava';
+import {listen} from './listen';
+
+test('listen an available port', async (t) => {
+    const port = 3000;
+    const servers: Array<http.Server> = [];
+    const ports: Array<number> = [port - 1];
+    for (let count = 0; count < 3; count++) {
+        const server = http.createServer();
+        servers.unshift(server);
+        const listening = await listen(server, port);
+        t.true(ports[0] < listening);
+        ports.unshift(listening);
+    }
+    await Promise.all(servers.map((server) => new Promise((resolve, reject) => {
+        server.close((error) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    })));
+});
