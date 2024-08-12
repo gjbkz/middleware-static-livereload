164e7be311f88dc34d79e9e7d43d3354e1f0ba10
Kei Ito <kei.itof@gmail.com>
2024-08-12 17:51:14 +0900

    test: use driveRoot

--- src/pathLikeToFileUrl.test.ts
+++ src/pathLikeToFileUrl.test.ts
@@ -1,12 +1,17 @@
 import * as assert from 'node:assert/strict';
+import { parse } from 'node:path';
 import { test } from 'node:test';
+import { pathToFileURL } from 'node:url';
 import { pathLikeToFileUrl } from './pathLikeToFileUrl.ts';
+const cwd = process.cwd();
+const driveRoot = parse(cwd).root;
+const rootUrl = pathToFileURL(driveRoot);
 
 test('absolute path', () => {
   const input = '/a';
   const baseDir = '/base';
   const actual = pathLikeToFileUrl(input, baseDir);
-  const expected = new URL('file:///a');
+  const expected = new URL('/a', rootUrl);
   assert.equal(actual.href, expected.href);
 });
 
@@ -14,7 +19,7 @@ test('relative path', () => {
   const input = 'a/b';
   const baseDir = '/base';
   const actual = pathLikeToFileUrl(input, baseDir);
-  const expected = new URL('file:///base/a/b');
+  const expected = new URL('/base/a/b', rootUrl);
   assert.equal(actual.href, expected.href);
 });
 
@@ -22,7 +27,7 @@ test('absolute path (buffer)', () => {
   const input = Buffer.from('/a');
   const baseDir = '/base';
   const actual = pathLikeToFileUrl(input, baseDir);
-  const expected = new URL('file:///a');
+  const expected = new URL('/a', rootUrl);
   assert.equal(actual.href, expected.href);
 });
 
@@ -30,8 +35,8 @@ test('relative path (buffer)', () => {
   const input = Buffer.from('a/b');
   const baseDir = '/base';
   const actual = pathLikeToFileUrl(input, baseDir);
-  const expected = new URL('file:///base/a/b');
-  assert.equal(actual.href, expected.href);
+  const expected = new URL('/base/a/b', rootUrl);
+  assert.ok(actual.href, expected.href);
 });
 
 test('url', () => {
